e7d3035bd54fdddee08b470e0458a512a89531c2
texetomg <texetomg@gmail.com>
2024-03-04 12:46:05 +0300

    Added streams.

--- javascript-5/streams.js
+++ javascript-5/streams.js
@@ -1,44 +1,45 @@
 import fs from 'fs'
 import readLine from 'readline'
 import _ from 'lodash'
+import { Transform } from 'stream'
 
 const inputFilePath = process.argv[2]
-
+const outputFilePath = 'output'
 const spaceRegex = /\s+/
 const wordsRegex = /[^a-zA-Z]/g
 
 const readStream = fs.createReadStream(inputFilePath);
+const writeStream = fs.createWriteStream(outputFilePath)
 
-const readLineInterface = readLine.createInterface({
-    input: readStream,
-})
 
-const wordsMap = {}
 
-readLineInterface.on('line', (line) => {
-    const words = line.split(spaceRegex)
-    const filteredWords = words.map(word => word.replace(wordsRegex, ''))
-   
-    filteredWords.forEach(filterdWord => {
-        if (wordsMap[filterdWord]) {
-            wordsMap[filterdWord] += 1
-        }
-        if (!wordsMap[filterdWord]) {
-            wordsMap[filterdWord] = 1
-        }
-    })
-})
-
-readLineInterface.on('close', () => {
+const countWordsTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const wordsMap = {}
+        const words = chunk.toString().split(spaceRegex)
+        const filteredWords = words.map(word => word.replace(wordsRegex, ''))
     
-    const result = _(wordsMap)
-        .toPairs()
-        .sortBy(0)
-        .fromPairs()
-        .values()
-        .value()
+        filteredWords.forEach(filterdWord => {
+            if (wordsMap[filterdWord]) {
+                wordsMap[filterdWord] += 1
+            }
+            if (!wordsMap[filterdWord]) {
+                wordsMap[filterdWord] = 1
+            }
+        })
 
-    console.log(result)
+        const result = _(wordsMap)
+            .toPairs()
+            .sortBy(0)
+            .fromPairs()
+            .values()
+            .value()
+            .toString()
+
+        callback(null, result)
+    }
 })
+
+readStream.pipe(countWordsTransform).pipe(writeStream)
 
 
